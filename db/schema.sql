e8e69da674478b529c1cb662494aada5481de6aa
ianhara <ianhara97@gmail.com>
2024-05-15 12:55:56 -0400

    added foreign keys and Db queries

--- db/schema.sql
+++ db/schema.sql
@@ -5,21 +5,27 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department(
-id INT,
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(100)
 );
 
 CREATE TABLE role(
-    id INT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT 
+    department_id INT,
+    foreign key(department_id)
+    references department(id)
 );
 
 CREATE TABLE employee(
-    id INT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT
+    manager_id INT,
+    foreign key (role_id)
+    references role(id),
+    foreign key (manager_id)
+    references employee(id)
 );
